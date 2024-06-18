da98cae10ff751d076a64ddb440ce3c67e07e5ec
AlfonzoRamiro <ramirobarriosalfonzo@gmail.com>
2024-06-18 19:58:18 -0300

    18 de junio

--- generales.py
+++ generales.py
@@ -3,26 +3,30 @@ import json
 import re
 import random
 
-lista_preguntas = []
+lista_preguntas_historia = []
+lista_preguntas_ciencia = []
+lista_preguntas_geografia = []
+lista_preguntas_deporte = []
+lista_preguntas_entretenimiento = []
 
 lista_preguntas_tocadas = []
 
 piramide_premios =  [
-    [1, 100, False],
-    [2, 200, False],
-    [3, 300, False],
-    [4, 500, False],
-    [5, 1000, False],
-    [6, 2000, False],
-    [7, 4000, False],
-    [8, 8000, False],
-    [9, 16000, False],
-    [10, 32000, False],
-    [11, 64000, False],
-    [12, 125000, False],
-    [13, 250000, False],
-    [14, 500000, False],
-    [15, 1000000, False]
+    ["Muy fácil", 100],
+    ["Muy fácil", 200],
+    ["Fácil", 300],
+    ["Fácil", 500],
+    ["Fácil", 1000],
+    ["Medio", 2000],
+    ["Medio", 4000],
+    ["Medio", 8000],
+    ["Medio", 16000],
+    ["Medio", 32000],
+    ["Dificil", 64000],
+    ["Dificil", 125000],
+    ["Dificil", 250000],
+    ["Muy dificil", 500000],
+    ["Muy dificil", 1000000]
 ]
 
 def desea_continuar(mensaje:str, mensaje_error: str) -> bool:
@@ -86,7 +90,8 @@ def comprobar_len_lista(lista: list) -> bool:
     
     return lenght
 
-def leer_preguntas_desde_csv(lista, archivo_csv):
+def leer_preguntas_desde_csv(lista_preguntas_historia, lista_preguntas_ciencia, lista_preguntas_geografia,
+                             lista_preguntas_deporte,  lista_preguntas_entretenimiento, archivo_csv):
     exito = True
     try:
         with open(archivo_csv, "r", encoding="utf8") as archivo:
@@ -94,8 +99,18 @@ def leer_preguntas_desde_csv(lista, archivo_csv):
                 registro = re.split(",|\n", linea)
                 
                 if registro[0] != "id":
-                    diccionario = crear_pregunta(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5])
-                    lista.append(diccionario)
+                    diccionario = crear_pregunta(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5], registro[6])
+                    match diccionario["categoria"]:
+                        case "Historia":
+                            lista_preguntas_historia.append(diccionario)
+                        case "Ciencia":
+                            lista_preguntas_ciencia.append(diccionario)
+                        case "Geografía":
+                            lista_preguntas_geografia.append(diccionario)
+                        case "Deporte":
+                            lista_preguntas_deporte.append(diccionario)
+                        case "Entretenimiento":
+                            lista_preguntas_entretenimiento.append(diccionario)
     except:
         exito = False
 
@@ -116,7 +131,7 @@ def cargar_paths(archivo_data):
         
     return lista_paths
         
-def crear_pregunta(id: int, categoria: str, dificultad: str, pregunta: str, respuestas: dict, respuesta_correcta: str) -> dict:
+def crear_pregunta(id: int, categoria: str, dificultad: str, pregunta: str, respuestas: dict, respuesta_correcta: str, palabra_clave: str) -> dict:
     diccionario_pregunta = {
         "id" : int(id),
         "categoria" : categoria,
@@ -124,6 +139,7 @@ def crear_pregunta(id: int, categoria: str, dificultad: str, pregunta: str, resp
         "pregunta" : pregunta,
         "respuestas" : respuestas,
         "respuesta_correcta" : int(respuesta_correcta),
+        "palabra_clave" : palabra_clave
         }
     
     return diccionario_pregunta
@@ -147,6 +163,12 @@ def elegir_pregunta_random(nivel: int, lista_preguntas_tocadas: list):
             case "Muy fácil":
                 while True:
                     id_random = random.randint(1,3)
+                    if id_random not in lista_preguntas_tocadas:
+                        id_retorno = id_random
+                        break
+            case "Fácil":
+                while True:
+                    id_random = random.randint(4,5)
                     if id_random not in lista_preguntas_tocadas:
                         id_retorno = id_random
                         break
@@ -198,136 +220,65 @@ def ayuda_del_publico(respuestas: list, indice_correcto: int):
     
     return prob_incorrectas
 
-def llamar_amigo(pregunta_tocada, respuestas):
-    respuesta_dada = pregunta_tocada["respuesta_correcta"]
-    print(respuestas[respuesta_dada])
-    return respuesta_dada
+def llamada(pregunta_tocada):
+    palabra_clave = pregunta_tocada["palabra_clave"]
+    print(f"-{palabra_clave}-")
+    return palabra_clave
 
-leer_preguntas_desde_csv(lista_preguntas, cargar_paths("paths.json")[0])
+leer_preguntas_desde_csv(lista_preguntas_historia, lista_preguntas_ciencia, lista_preguntas_geografia,
+                         lista_preguntas_deporte,  lista_preguntas_entretenimiento, cargar_paths("paths.json")[0])
+bandera_50_50 = True
+bandera_publico = True
+bandera_llamada = True
+m = 0
+
 
 while True:
-    id_pregunta = elegir_pregunta_random("Muy fácil", lista_preguntas_tocadas)
-    #print(id_pregunta)
-    pregunta_tocada = mostrar_pregunta(lista_preguntas, id_pregunta)
+    nivel = piramide_premios[m][0]
+    id_pregunta = elegir_pregunta_random(nivel, lista_preguntas_tocadas)
+    pregunta_tocada = mostrar_pregunta(lista_preguntas_historia, id_pregunta)
     respuestas = pregunta_tocada["respuestas"].split("-")
     respuesta_correcta = pregunta_tocada["respuesta_correcta"]
-    print("1. 50-50%")
-    print("2. Público")
-    print("3. Amigo")
-    print("4. Salir")
-    comodin = seleccionar_opcion_menu("Selecciones una opción: ")
-    match comodin:
-        case 1:
-            print(realizar_50_50(respuestas, respuesta_correcta))
-        case 2:
-            print(ayuda_del_publico(respuestas, respuesta_correcta))
-        case 3:
-            llamar_amigo(pregunta_tocada, respuestas)
-        case 4:
-            pass
-        case _:
-            print("Elija una opción válida.")
+    
+    
+    seguir_comodines = True
+    while seguir_comodines:
+        print("1. 50-50%")
+        print("2. Público")
+        print("3. Amigo")
+        print("4. Salir")
+        comodin = seleccionar_opcion_menu("Selecciones una opción: ")
+        
+        match comodin:
+            case 1:
+                if bandera_50_50:
+                    print(realizar_50_50(respuestas, respuesta_correcta))
+                    bandera_50_50 = False
+                else:
+                    print("No tiene mas el comodín 50-50%")
+            case 2:
+                if bandera_publico:
+                    print(ayuda_del_publico(respuestas, respuesta_correcta))
+                    bandera_publico = False
+                else:
+                    print("No tiene mas el comodín del público")
+            case 3:
+                if bandera_llamada:
+                    llamada(pregunta_tocada)
+                    bandera_llamada = False
+                else:
+                    print("No tiene mas el comodín de llamada")
+            case 4:
+                if desea_continuar("Seguro que no quiere uitilizar comodines? SI/NO: ", "Error. Ingrese SI/NO: "):
+                    seguir_comodines = False
+            case _:
+                print("Elija una opción válida.")
 
     opcion_elegida = seleccionar_opcion_menu("Seleccione una opción, 1-2-3-4: ") - 1
     if opcion_elegida == pregunta_tocada["respuesta_correcta"]:
-        piramide_premios[0][2] = True
+        m += 1
         lista_preguntas_tocadas.append(pregunta_tocada["id"])
         print("Respuesta correcta.")
-        
     else:
         print("Respuesta incorrecta, ha perdido.")
         break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Ejemplo de uso:
-# archivo_csv = "preguntas.csv"
-# leer_preguntas_desde_csv(lista_preguntas, archivo_csv)
-# # for pregunta in lista_preguntas:
-# #     print(f"Categoría: {pregunta['categoria']}")
-# #     print(f"Dificultad: {pregunta['dificultad']}")
-# #     print(f"Pregunta: {pregunta['pregunta']}")
-# #     print(f"Opciones: {pregunta['opciones']}")
-# #     print(f"Respuesta correcta: {pregunta['respuesta_correcta']}")
-# #     print("=" * 50)
-# print(lista_preguntas)
