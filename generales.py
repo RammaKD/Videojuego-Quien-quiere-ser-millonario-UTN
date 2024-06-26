a8b34471d070c4b4d94850000f89c8a34be786da
AlfonzoRamiro <ramirobarriosalfonzo@gmail.com>
2024-06-25 21:17:09 -0300

    Cambios generales

--- generales.py
+++ generales.py
@@ -1,8 +1,6 @@
 import json
-import re
 import random
-import os
-
+import pygame
 
 def desea_continuar(mensaje:str, mensaje_error: str) -> bool:
     """Pregunta al usuario si quiere continuar con una determinada funcion o no.
@@ -65,119 +63,118 @@ def comprobar_len_lista(lista: list) -> bool:
     
     return lenght
 
-def leer_preguntas_desde_csv(lista_preguntas, categoria_elegida, archivo_csv):
+def cargar_billetera_json(ruta_archivo_json):
+    billetera = True
+    try:
+        with open(ruta_archivo_json, 'r') as file:
+            billetera = json.load(file)["billetera"]
+    except:
+        billetera = False
+    
+    return billetera
+
+def actualizar_billetera_json(ruta_archivo_json, puntuacion_actualizada):
     exito = True
     try:
-        with open(archivo_csv, "r", encoding="utf8") as archivo:
-            for linea in archivo:
-                registro = re.split(",|\n", linea)
-                
-                if registro[0] != "id":
-                    diccionario = crear_pregunta(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5], registro[6])
-                    if diccionario["categoria"] == categoria_elegida:
-                        lista_preguntas.append(diccionario)
-    except Exception as e:
-        print(f"Se ha producido una excepcio {e}")
-        exito = False
-
-    return exito
-
-def cargar_puntuaciones_json(archivo_json):
-    with open(archivo_json, 'r') as file:
-        puntuaciones = json.load(file)["premio"]
-    return puntuaciones
-
-def actualizar_puntuaciones_json(archivo_json, puntuacion_actualizada):
-    exito = True
-    try:
-        with open(archivo_json, "w") as archivo:
-            nueva_puntuacion = {"premio" : puntuacion_actualizada}
+        with open(ruta_archivo_json, "w") as archivo:
+            nueva_puntuacion = {"billetera" : puntuacion_actualizada}
             json.dump(nueva_puntuacion, archivo, indent=2)
     except:
         exito = False
     
     return exito
 
-def cargar_paths(archivo_data):
-    archivo_data = os.path.abspath(archivo_data)
-    with open(archivo_data, "r") as file:
-        diccionario_paths = json.load(file)
-        for path in diccionario_paths:
-            archivo_preguntas = diccionario_paths[path]["path_preguntas"]
-            archivo_premios = diccionario_paths[path]["path_premios"]
-            archivo_fondo_menu = diccionario_paths[path]["path_fondo_menu"]
-            archivo_logo = diccionario_paths[path]["path_logo"]
-            archivo_fondo_juego = diccionario_paths[path]["path_fondo_juego"]
-            archivo_presentador = diccionario_paths[path]["path_presentador"]
-            archivo_cuadro_de_texto = diccionario_paths[path]["path_cuadro_texto"]
-        lista_paths = [archivo_preguntas, archivo_premios, archivo_fondo_menu, archivo_logo, archivo_fondo_juego, archivo_presentador, archivo_cuadro_de_texto]
-        
-    return lista_paths
-        
-def crear_pregunta(id: int, categoria: str, dificultad: str, pregunta: str, respuestas: dict, respuesta_correcta: str, palabra_clave: str) -> dict:
-    diccionario_pregunta = {
-        "id" : int(id),
-        "categoria" : categoria,
-        "dificultad" : dificultad,
-        "pregunta" : pregunta,
-        "respuestas" : respuestas,
-        "respuesta_correcta" : int(respuesta_correcta),
-        "palabra_clave" : palabra_clave
-        }
+def obtener_paths(ruta_archivo_json):
+    with open(ruta_archivo_json, "r", encoding="utf-8") as archivo:
+        data = json.load(archivo)
+    diccionario_paths = {}
     
-    return diccionario_pregunta
+    for clave, valor in data["paths"].items():
+        diccionario_paths[clave] = valor
+    
+    return diccionario_paths
+        
+def leer_preguntas_csv(ruta_archivo_csv):
+    lista_datos_csv = []
+    with open(ruta_archivo_csv, "r", encoding="utf-8") as archivo:
+        lineas = archivo.readlines()
+        encabezado = lineas[0].strip().split(',')
+        lista_datos_csv.append(encabezado)
+        
+        valores_preguntas = []
+        for linea in lineas[1:]:
+            valores_preguntas.append(linea.strip().split(','))
+        
+        lista_datos_csv.append(valores_preguntas)
+    
+    return lista_datos_csv
 
-def mostrar_pregunta(lista_preguntas, id_pregunta):
+def crear_diccionario_preguntas(lista_datos, lista_preguntas):
+    for valores in lista_datos[1]:
+        pregunta = {}
+        for i in range(len(lista_datos[0])):
+            pregunta[lista_datos[0][i]] = valores[i]
+        lista_preguntas.append(pregunta)
+    
+    return lista_preguntas
+
+def cargar_posibles_preguntas(lista_preguntas, lista_preguntas_superadas, categoria_elegida, nivel):
+    lista_preguntas_posibles = []
+    for pregunta in lista_preguntas:
+        if pregunta["Categoría"] == categoria_elegida and pregunta["Nivel"] == nivel and pregunta not in lista_preguntas_superadas:
+           lista_preguntas_posibles.append(pregunta)
+
+    return lista_preguntas_posibles
+
+def cargar_pregunta_aleatoriamente(lista_preguntas):
+    pregunta_aleatoria = random.choice(lista_preguntas)
+    return pregunta_aleatoria
+           
+def crear_lista_respuestas(pregunta):
+    lista_respuestas = pregunta["Respuestas"].split("-")
+    return lista_respuestas
+    
+def cargar_pantalla_menu(ventana_principal, elementos):
+    exito = True
     try:
-        for pregunta in lista_preguntas:
-            if pregunta["id"] == id_pregunta:
-                print(pregunta["pregunta"])
-                print(pregunta["respuestas"])
-                retorno = pregunta
-                break
+        for elemento in elementos:
+            imagen = elemento[0]
+            coordenadas = elemento[1]
+            ventana_principal.blit(imagen, coordenadas)
     except:
-        retorno = False
+        exito = False
+    
+    return exito
 
-    return retorno
 
-def elegir_pregunta_random(nivel: int, lista_preguntas_tocadas: list):
-    try:
-        match nivel:
-            case "Muy fácil":
-                while True:
-                    id_random = random.randint(1,4)
-                    if id_random not in lista_preguntas_tocadas:
-                        id_retorno = id_random
-                        break
-            case "Fácil":
-                while True:
-                    id_random = random.randint(5,10)
-                    if id_random not in lista_preguntas_tocadas:
-                        id_retorno = id_random
-                        break
-            case "Medio":
-                while True:
-                    id_random = random.randint(11,21)
-                    if id_random not in lista_preguntas_tocadas:
-                        id_retorno = id_random
-                        break
-            case "Difícil":
-                while True:
-                    id_random = random.randint(22,27)
-                    if id_random not in lista_preguntas_tocadas:
-                        id_retorno = id_random
-                        break
-            case "Muy difícil":
-                while True:
-                    id_random = random.randint(28,32)
-                    if id_random not in lista_preguntas_tocadas:
-                        id_retorno = id_random
-                        break
-    except:
-        id_retorno = False
-    return id_retorno
 
-def realizar_50_50(respuestas: list, respuesta_correcta: int):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#REVEERLAS#####################################################################
+def activar__50_50(respuestas: list, respuesta_correcta: int):
     respuestas_incorrectas = []
     
     for respuesta in respuestas:
@@ -191,7 +188,7 @@ def realizar_50_50(respuestas: list, respuesta_correcta: int):
     
     return respuestas_finales
 
-def ayuda_del_publico(respuestas: list, indice_correcto: int):
+def activar_ayuda_del_publico(respuestas: list, indice_correcto: int):
     num_respuestas = len(respuestas)
     
     # Probabilidad base para la respuesta correcta (50% a 70%)
@@ -221,83 +218,9 @@ def ayuda_del_publico(respuestas: list, indice_correcto: int):
     
     return prob_incorrectas
 
-def llamada(pregunta_tocada):
-    palabra_clave = pregunta_tocada["palabra_clave"]
-    print(f"-{palabra_clave}-")
-    return palabra_clave
 
-def ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero):
-    bandera_50_50 = True
-    bandera_publico = True
-    bandera_llamada = True
-    m = 0
-    n = -1
-    dinero_acumulado = 0
-    
-    while True:
-        nivel = piramide_premios[m][0]
-        id_pregunta = elegir_pregunta_random(nivel, lista_preguntas_tocadas)
-        pregunta_tocada = mostrar_pregunta(lista_preguntas, id_pregunta)
-        respuestas = pregunta_tocada["respuestas"].split("-")
-        respuesta_correcta = pregunta_tocada["respuesta_correcta"]
-        
-        seguir_comodines = True
-        while seguir_comodines:
-            print("1. 50-50%")
-            print("2. Público")
-            print("3. Amigo")
-            print("4. Salir")
-            comodin = seleccionar_opcion_menu("Selecciones una opción: ")
-            
-            match comodin:
-                case 1:
-                    if bandera_50_50:
-                        print(realizar_50_50(respuestas, respuesta_correcta))
-                        bandera_50_50 = False
-                    else:
-                        print("No tiene mas el comodín 50-50%")
-                case 2:
-                    if bandera_publico:
-                        print(ayuda_del_publico(respuestas, respuesta_correcta))
-                        bandera_publico = False
-                    else:
-                        print("No tiene mas el comodín del público")
-                case 3:
-                    if bandera_llamada:
-                        llamada(pregunta_tocada)
-                        bandera_llamada = False
-                    else:
-                        print("No tiene mas el comodín de llamada")
-                case 4:
-                    if desea_continuar("Seguro que no quiere uitilizar comodines? SI/NO: ", "Error. Ingrese SI/NO: "):
-                        seguir_comodines = False
-                case _:
-                    print("Elija una opción válida.")
 
-        opcion_elegida = seleccionar_opcion_menu("Seleccione una opción, 1-2-3-4: ") - 1
-        if opcion_elegida == pregunta_tocada["respuesta_correcta"]:
-            m += 1
-            n += 1
-            dinero_acumulado += piramide_premios[n][1]
-            lista_preguntas_tocadas.append(pregunta_tocada["id"])
-            print("Respuesta correcta.")
-            print(f"Usted tiene acumulado: ${dinero_acumulado}")
-            if m == 15:
-                billetera += dinero_acumulado
-                actualizar_puntuaciones_json(path_dinero, billetera)
-                print("Felicidades, ha ganado el millón!")
-                print(f"Billetera: ${billetera}")
-                break
-            elif not desea_continuar("Desea seguir jugando? SI/NO: ", "Error. Ingrese SI/NO: "):
-                billetera += dinero_acumulado
-                actualizar_puntuaciones_json(path_dinero, billetera)
-                print(f"Usted ha ganado: ${dinero_acumulado}")
-                print(f"Billetera: ${billetera}")
-                break
-        else:
-            print("Respuesta incorrecta, ha perdido.")
-            print(f"Billetera: ${billetera}")
-            break
+
 
 
 
--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import pygame
 from generales import *
 
 lista_preguntas = []
-lista_preguntas_tocadas = []
-paths = cargar_paths("archivos\paths.json")
-path_preguntas = paths[0]
-path_dinero = paths[1]
-path_fondo_menu = paths[2]
-path_logo = paths[3]
-path_fondo_juego = paths[4]
-path_presentador = paths[5]
-path_cuadro_de_texto = paths[6]
+lista_preguntas_superadas = []
+lista_categorias = ["Historia", "Deporte", "Ciencia", "Entretenimiento", "Geografía"]
+diccionario_paths = obtener_paths("archivos\\paths.json")
+lista_datos_csv = leer_preguntas_csv(diccionario_paths["path_preguntas"])
+crear_diccionario_preguntas(lista_datos_csv, lista_preguntas)
+lista_posibles_preguntas = cargar_posibles_preguntas(lista_preguntas, lista_preguntas_superadas, "Historia", "1")
+pregunta_cargada = cargar_pregunta_aleatoriamente(lista_posibles_preguntas)
+lista_respuestas = crear_lista_respuestas(pregunta_cargada)
 
-# PYGAME
+#PYGAME
 NEGRO = (0, 0, 0)
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
@@ -20,166 +19,39 @@ VERDE = (0, 255, 0)
 AZUL_CLARO = (0, 150, 255)
 BLANCO = (255, 255, 255)
 
-FPS = 60
 ANCHO_VENTANA = 1280
 ALTO_VENTANA = 720
 DIMENSIONES_VENTANA = (ANCHO_VENTANA, ALTO_VENTANA)
-DIMENSIONES_CUADRO_TEXTO = 280,70
+DIMENSIONES_BOTON = (280,70)
 
 pygame.init()
 
 ventana_principal = pygame.display.set_mode(DIMENSIONES_VENTANA)
-pygame.display.set_caption("Quien quiere ser millonario?")
-ventana_principal.fill(AZUL_CLARO)
-fuente_menu_play = pygame.font.SysFont("sinsum", 75)
-fuente_menu_categoria = pygame.font.SysFont("mvboli", 65)
-
-fondo_menu = pygame.image.load(path_fondo_menu)
+ventana_principal.fill(BLANCO)
+logo = pygame.image.load(diccionario_paths["path_logo"])
+logo = pygame.transform.scale(logo, (400,350))
+fondo_menu = pygame.image.load(diccionario_paths["path_fondo_menu"])
 fondo_menu = pygame.transform.scale(fondo_menu, DIMENSIONES_VENTANA)
-logo = pygame.image.load(path_logo)
-logo = pygame.transform.scale(logo, (370, 320))
+boton = pygame.image.load(diccionario_paths["path_boton"])
+boton = pygame.transform.scale(boton, DIMENSIONES_BOTON)
+fuente = pygame.font.SysFont("sinsum", 75)
+texto_play = fuente.render("JUGAR", False, BLANCO)
+texto_salir = fuente.render("SALIR", False, BLANCO)
 
-texto_play = fuente_menu_play.render("JUGAR", False, BLANCO)
-texto_exit = fuente_menu_play.render("SALIR", False, BLANCO)
-texto_categoria = fuente_menu_categoria.render("Elija una categoría", False, NEGRO)
+pygame.display.set_caption("¿Quien quiere ser millonario?")
+pygame.display.set_icon(logo)
 
+lista_elementos_pantalla_menu = [(fondo_menu, (0,0)), (logo, (450,75)), (boton, (330,500)), (boton, (690,500)),
+                                 (texto_play, (380,515)), (texto_salir, (750,515))]
 
-cuadro_texto = pygame.image.load(path_cuadro_de_texto)
-cuadro_texto = pygame.transform.scale(cuadro_texto, (DIMENSIONES_CUADRO_TEXTO))
-cuadro_texto_rect_play = cuadro_texto.get_rect()
-cuadro_texto_rect_play.topleft = (330,465)
-cuadro_texto_rect_exit = cuadro_texto.get_rect()
-cuadro_texto_rect_exit.topleft = (690,465)
-
-
-def cargar_pantalla_menu_principal(fondo_menu, logo, cuadro_texto, texto_play, texto_exit):
-    ventana_principal.blit(fondo_menu, (0,0))
-    ventana_principal.blit(logo, (480, 75))
-    ventana_principal.blit(cuadro_texto, (330,465))
-    ventana_principal.blit(cuadro_texto, (690,465))
-    ventana_principal.blit(texto_play, (380,480))
-    ventana_principal.blit(texto_exit, (750,480))
-
-def cargar_pantalla_elegir_categoria(fondo_menu, logo):
-    ventana_principal.blit(fondo_menu, (0,0))
-    ventana_principal.blit(logo, (480,30))
-    pygame.draw.rect(ventana_principal, BLANCO, (390,360,570,60))
-    ventana_principal.blit(texto_categoria, (400,333))
-    
-
-
-
-tiempo_inicial = pygame.time.get_ticks()
-mostrar_menu = True
-boton_salir = True
 flag_run = True
-
 while flag_run:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             flag_run = False
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            print(evento.pos)
-            if cuadro_texto_rect_play.collidepoint(evento.pos):
-                mostrar_menu = False
-                boton_salir = False
-                cargar_pantalla_elegir_categoria(fondo_menu, logo)
-            elif cuadro_texto_rect_exit.collidepoint(evento.pos) and boton_salir == True:
-                flag_run = False
-    
-    if mostrar_menu:
-        cargar_pantalla_menu_principal(fondo_menu, logo, cuadro_texto, texto_play, texto_exit)
-    
+
+    cargar_pantalla_menu(ventana_principal, lista_elementos_pantalla_menu)
     pygame.display.update()
 
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-# tiempo_actual = pygame.time.get_ticks()
-# tiempo_transcurrido = (tiempo_actual - tiempo_inicial) * 0.001
-# if tiempo_transcurrido > 5.00:
-#     flag_run = False
-
-# piramide_premios =  [
-#     ["Muy fácil", 100],
-#     ["Muy fácil", 200],
-#     ["Fácil", 300],
-#     ["Fácil", 500],
-#     ["Fácil", 1000],
-#     ["Medio", 2000],
-#     ["Medio", 4000],
-#     ["Medio", 8000],
-#     ["Medio", 16000],
-#     ["Medio", 32000],
-#     ["Dificil", 64000],
-#     ["Dificil", 125000],
-#     ["Dificil", 250000],
-#     ["Muy dificil", 500000],
-#     ["Muy dificil", 1000000]
-#]
-
-# seguir = True
-# while seguir:
-#     print("1. JUGAR")
-#     print("2. SALIR")
-#     opcion = seleccionar_opcion_menu("Elija una opción: ")
-
-#     match opcion:
-#         case 1:
-#             seguir_eligiendo = True
-#             while seguir_eligiendo:
-#                 print("1. Historia")
-#                 print("2. Deporte")
-#                 print("3. Entretinimiento")
-#                 print("4. Ciencia")
-#                 print("5. Geografía")
-#                 print("6. Salir")
-#                 lista_categorias = ["Historia", "Deporte", "Entretenimiento", "Ciencia", "Geografía"]
-#                 opcion_categoria = seleccionar_opcion_menu("Elija una opción: ")
-#                 lista_preguntas.clear()
-#                 if opcion_categoria != 6:
-#                     categoria_elegida = lista_categorias[opcion_categoria - 1]
-#                     leer_preguntas_desde_csv(lista_preguntas, categoria_elegida, path_preguntas)
-#                     billetera = cargar_puntuaciones_json(path_dinero)
-#                     print(f"Billetera: ${billetera}")
-                
-#                 match opcion_categoria:
-#                     case 1:
-#                         ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero)
-#                         break
-#                     case 2: 
-#                         ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero)
-#                         break
-#                     case 3:
-#                         ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero)
-#                         break
-#                     case 4:
-#                         ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero)
-#                         break
-#                     case 5:
-#                         ejecutar_juego(lista_preguntas, lista_preguntas_tocadas, piramide_premios, billetera, path_dinero)
-#                         break
-                        
-#                     case 6:
-#                         if desea_continuar("Desea dejar de elegir? SI/NO: ", "Error. Ingrese SI/NO: "):
-#                             seguir_eligiendo = False
-#                     case _:
-#                         print("Error. Ingrese una opción del Menú.")
-                        
-#         case 2:
-#             if desea_continuar("Desea salir? SI/NO: ", "Error. Ingrese SI/NO: "):
-#                 seguir = False
-#                 break
-#         case _:
-#             print("Error. Ingrese una opción del Menú.")
-
 
